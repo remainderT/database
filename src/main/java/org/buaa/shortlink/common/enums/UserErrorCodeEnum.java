da8a35efe756fc5b169769c5056c4fd69082ab78
ysh <102450911+ysh365@users.noreply.github.com>
2024-11-04 12:32:50 +0800

    增加用户错误码

--- src/main/java/org/buaa/shortlink/common/enums/UserErrorCodeEnum.java
+++ src/main/java/org/buaa/shortlink/common/enums/UserErrorCodeEnum.java
@@ -7,7 +7,25 @@ import org.buaa.shortlink.common.convention.errorcode.IErrorCode;
  */
 public enum UserErrorCodeEnum implements IErrorCode {
 
-    USER_NULL("A000200", "用户记录不存在");
+    USER_NAME_EXIST("A000101", "用户名已存在"),
+
+    USER_EXIST("A000102", "用户记录已存在"),
+
+    USER_SAVE_ERROR("A000103", "用户记录新增失败"),
+
+    USER_CODE_ERROR("A000104", "验证码错误"),
+
+    USER_NAME_NULL("A000201", "用户名不存在"),
+
+    USER_PASSWORD_ERROR("A000202", "密码错误"),
+
+    USER_REPEATED_LOGIN("A000203", "重复登录"),
+
+    USER_TOKEN_NULL("A000204", "用户未登录"),
+
+    USER_NULL("A000301", "用户记录不存在"),
+
+    USER_UPDATE_ERROR("A000302", "用户信息更新失败");
 
 
     private final String code;
